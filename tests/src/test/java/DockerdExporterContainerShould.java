640eef862c2130eacef744097c24875b392527c4
Lukas Holota <l.holota@outlook.com>
2020-06-18 14:02:22 +0200

    fix

--- tests/src/test/java/DockerdExporterContainerShould.java
+++ tests/src/test/java/DockerdExporterContainerShould.java
@@ -9,7 +9,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import java.io.IOException;
